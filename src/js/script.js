2774f3342d9812f61f226d2aa7a494706b9f2040
Allah Macedo <allah.coder@gmail.com>
2022-03-19 23:29:48 -0300

    Feito o component change thumb e sua mecanica

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function setCircleBGC(color) {
+    const circleColor = document.querySelector('.circle')
+    circleColor.style.background = color; 
+}
+
+function ChangeProductImg(image) {
+    document.querySelector('.product').src = image
+}
